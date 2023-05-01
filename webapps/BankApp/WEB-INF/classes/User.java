bbf712a8d690e526720b88222b1149964221aa70
gveasyJr <geoflveas96@gmail.com>
2023-05-01 05:20:00 -0500

    Added get method, delete account method and list object to User.java

--- webapps/BankApp/WEB-INF/classes/User.java
+++ webapps/BankApp/WEB-INF/classes/User.java
@@ -1,4 +1,9 @@
 import java.util.List;
+import java.util.ArrayList;
+//A "User" object will be used to represent a single bank user:
+//Example: John Smith
+//A User should have: An ID --> A Username --> A list of accounts
+//A User should be able to: Add and Remove accounts
 
 public class User {
     int ID;
@@ -8,6 +13,7 @@ public class User {
     public User(String username, int id){
         this.username = username;
         this.ID = id;
+        this.accounts = new ArrayList<Account>();
     }
 
     public void addAccount(Account account){
@@ -15,6 +21,23 @@ public class User {
     }
 
     public void deleteAccount(Account account){
-        accounts.remove(account);
+        try{
+            accounts.remove(account);
+        }
+        catch (IndexOutOfBoundsException ex){
+            System.out.println("There are no accounts to delete");
+        }
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public List<Account> getAccounts(){
+        return accounts;
     }
 }
